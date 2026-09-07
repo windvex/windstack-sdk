0dc82fdd35195452ad9b00822bfcc5d05f51eb12
Wind Crypto | Vexanium <0xgvexa@gmail.com>
2026-09-07 10:49:13 +0700

    refactor(vexanium): export native VSR option types

--- packages/vexanium/src/index.ts
+++ packages/vexanium/src/index.ts
@@ -61,6 +61,7 @@ export type {
   VexSigningRequestParams,
   VexSigningRequestResult,
   VexSigningRequestUri,
+  VexSigningRequestZlibProvider,
   VexaniumAccount,
   VexaniumAccountsResponse,
   VexaniumCapabilitiesRequest,
