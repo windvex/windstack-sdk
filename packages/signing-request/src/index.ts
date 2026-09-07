62549c21e36733e559dc4c17aef1a536e580b808
Wind Crypto | Vexanium <0xgvexa@gmail.com>
2026-09-07 10:43:47 +0700

    feat(signing-request): expose public API

---
+++ packages/signing-request/src/index.ts
@@ -0,0 +1,58 @@
+/**
+ * WindStack Antelope SDK
+ * Created by Gilang Ramadan
+ * Copyright (c) 2026 PT WIND KRIPTOGRAFI TEKNOLOGI
+ * SPDX-License-Identifier: MIT
+ */
+export { encodeBase64Url, decodeBase64Url } from "./base64url.js";
+export {
+  pakoCompressionProvider,
+  type CompressionProvider,
+} from "./compression.js";
+export {
+  SIGNING_REQUEST_FLAG_BACKGROUND,
+  SIGNING_REQUEST_FLAG_BROADCAST,
+  SIGNING_REQUEST_MAX_DECODED_BYTES,
+  SIGNING_REQUEST_MIN_SUPPORTED_VERSION,
+  SIGNING_REQUEST_PLACEHOLDER_ACTOR,
+  SIGNING_REQUEST_PLACEHOLDER_PERMISSION,
+  SIGNING_REQUEST_PROTOCOL_VERSION,
+  SigningRequest,
+  createSigningRequest,
+  parseSigningRequest,
+} from "./request.js";
+export {
+  RpcSigningRequestAbiProvider,
+  resolveSigningRequest,
+  resolveSigningRequestWithRpc,
+} from "./resolution.js";
+export {
+  createSigningRequestCallback,
+  verifyResolvedSigningRequestSignature,
+} from "./callback.js";
+export { SIGNING_REQUEST_ABI } from "./schema.js";
+export type {
+  ResolvedSigningRequest,
+  SigningRequestAbiProvider,
+  SigningRequestAction,
+  SigningRequestActionInput,
+  SigningRequestCallback,
+  SigningRequestCallbackContext,
+  SigningRequestChain,
+  SigningRequestCreateArguments,
+  SigningRequestData,
+  SigningRequestEncodingOptions,
+  SigningRequestExtension,
+  SigningRequestIdentity,
+  SigningRequestInfoInput,
+  SigningRequestInfoPair,
+  SigningRequestParseOptions,
+  SigningRequestPayload,
+  SigningRequestPermissionLevel,
+  SigningRequestResolveOptions,
+  SigningRequestScheme,
+  SigningRequestSignature,
+  SigningRequestTapos,
+  SigningRequestTransaction,
+  SigningRequestTransactionInput,
+} from "./types.js";
