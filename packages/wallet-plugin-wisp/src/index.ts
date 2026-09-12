94226e26d98ffeb979a15d5df89ac2dfa2898faf
Wind Crypto | Vexanium <0xgvexa@gmail.com>
2026-09-12 10:47:49 +0700

    feat(wisp): export Telegram connection state errors

--- packages/wallet-plugin-wisp/src/index.ts
+++ packages/wallet-plugin-wisp/src/index.ts
@@ -1,6 +1,11 @@
 export { WispWalletPlugin } from "./WispWalletPlugin.js";
 export type { WispWalletPluginOptions } from "./WispWalletPlugin.js";
-export { createWispTelegramTransport } from "./WispTelegramTransport.js";
+export {
+  createWispTelegramTransport,
+  WispTelegramAlreadyConnectedError,
+  WispTelegramNotConnectedError,
+  WispTelegramRestoreRequiredError,
+} from "./WispTelegramTransport.js";
 export type {
   WispTelegramDappMetadata,
   WispTelegramSession,
