c32674f151215383b3751ad37612763142c78220
Wind Crypto | Vexanium <0xgvexa@gmail.com>
2026-09-12 08:23:42 +0700

    feat(wisp): export Telegram transport

--- packages/wallet-plugin-wisp/src/index.ts
+++ packages/wallet-plugin-wisp/src/index.ts
@@ -1,2 +1,11 @@
 export { WispWalletPlugin } from "./WispWalletPlugin.js";
 export type { WispWalletPluginOptions } from "./WispWalletPlugin.js";
+export { createWispTelegramTransport } from "./WispTelegramTransport.js";
+export type {
+  WispTelegramDappMetadata,
+  WispTelegramSession,
+  WispTelegramSessionStorage,
+  WispTelegramTransactArgs,
+  WispTelegramTransport,
+  WispTelegramTransportOptions,
+} from "./WispTelegramTransport.js";
